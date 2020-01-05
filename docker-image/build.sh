5d12046890db21494ac4afd420b771f012a43458
Stéphane Klein <contact@stephane-klein.info>
2020-01-05 16:49:56 +0100

    Improve Dockerfile to build frontend inside Docker build stage

--- docker-image/build.sh
+++ docker-image/build.sh
@@ -4,12 +4,6 @@ set -e
 cd $(dirname "$0")
 
 (
-    cd ../frontend
-    unset REACT_APP_API_URL
-    npm run build
-)
-
-(
     cd ..
-    docker build . -f docker-image/Dockerfile -t harobed/gibbon-mail:latest
+    docker build . -f docker-image/Dockerfile -t stephaneklein/gibbon-mail:latest
 )
