bffabc9825370015796d4479fbf533364cb6795c
ItsAlex <me@itsalex.fr>
2021-04-19 22:49:41 +0200

    fix: make backend listen on 0.0.0.0

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ const app = createApp();
 
 const server = app.listen(
     config.get('port'),
+    '0.0.0.0',
     () => {
         console.log(`Server listening on port: ${config.get('port')}`);
     }
